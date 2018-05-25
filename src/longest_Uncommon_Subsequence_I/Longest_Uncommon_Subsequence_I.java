3a3e09a021147f8f8ee19fba33e5afb65b5f196e
kent-syw <695540301@qq.com>
2018-05-25 08:00:42 +0800

    最长特殊序列1

---
+++ src/longest_Uncommon_Subsequence_I/Longest_Uncommon_Subsequence_I.java
@@ -0,0 +1,19 @@
+package longest_Uncommon_Subsequence_I;
+
+public class Longest_Uncommon_Subsequence_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution solution = new Solution();
+		System.out.println(solution.findLUSlength("", ""));
+	}
+}
+	class Solution {
+		public int findLUSlength(String a, String b) {
+			int value = 0;
+
+			return value;
+
+		}
+	}
+
